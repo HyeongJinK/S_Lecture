ceae0bf9f7bda36b1a21ecbf5a3f79148d3ed4e1
napasun <wkzkfmxk23@gmail.com>
2020-02-20 02:44:49 +0900

    공부중

--- src/main/java/khj/study/security/configuration/SecurityConfig.java
+++ src/main/java/khj/study/security/configuration/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("test@test.com")
-                .password("test").roles("USER");
+                .password("{noop}test").roles("USER");
     }
 
     @Override
