f1181efc2c378401fa2c7533d5047b4b3c76bd94
HyeongJinK <wkzkfmxk23@gmail.com>
2021-05-16 22:48:16 +0900

    [feat] series project create

---
+++ series/src/main/kotlin/khj/side/series/SeriesApplication.kt
@@ -0,0 +1,11 @@
+package khj.side.series
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeriesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeriesApplication>(*args)
+}
---
+++ series/src/test/kotlin/khj/side/series/SeriesApplicationTests.kt
@@ -0,0 +1,13 @@
+package khj.side.series
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeriesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
