11a4251776447576f0ad6c11adbfc0d4218cca84
HyeongJinK <wkzkfmxk23@gmail.com>
2021-05-27 22:23:15 +0900

    .

--- board/src/main/java/khj/side/board/dto/BoardContentDto.java
+++ board/src/main/java/khj/side/board/dto/BoardContentDto.java
@@ -1,4 +1,13 @@
 package khj.side.board.dto;
 
+import java.time.LocalDateTime;
+
 public class BoardContentDto {
+    Long boardContentIdx;
+    String subject;
+    String content;
+    LocalDateTime createDate;
+    LocalDateTime updateDate;
+    int readCount;
+    boolean isNew;
 }
