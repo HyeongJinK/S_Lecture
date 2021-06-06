578537ac43e54a4fa89b02f7056c24dde3214ced
HyeongJinK <wkzkfmxk23@gmail.com>
2021-06-06 23:12:19 +0900

    .

--- board/src/main/java/khj/side/board/entity/BoardTag.java
+++ board/src/main/java/khj/side/board/entity/BoardTag.java
@@ -1,8 +1,12 @@
 package khj.side.board.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardTag {
     Long idx;
     Long boardContentIdx;
