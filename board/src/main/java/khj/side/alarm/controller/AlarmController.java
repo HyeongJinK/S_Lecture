dd1b806374a4db0e8cc76cd8fc1e2f6aa0aab07a
HyeongJinK <wkzkfmxk23@gmail.com>
2021-06-23 23:36:19 +0900

    .

---
+++ board/src/main/java/khj/side/alarm/controller/AlarmController.java
@@ -0,0 +1,8 @@
+package khj.side.alarm.controller;
+
+public interface AlarmController {
+    void list();
+    void read();
+    void send();
+    void del();
+}
---
+++ board/src/main/java/khj/side/alarm/service/AlarmService.java
@@ -0,0 +1,8 @@
+package khj.side.alarm.service;
+
+public interface AlarmService {
+    void list();
+    void read();
+    void send();
+    void del();
+}
