311f2aee9d0ef3cb8d197d38e331a1a98ca84f39
HyeongJinK <wkzkfmxk23@gmail.com>
2021-06-24 23:37:47 +0900

    .

---
+++ board/src/main/java/khj/side/alarm/entity/Alarm.java
@@ -0,0 +1,10 @@
+package khj.side.alarm.entity;
+
+import java.time.LocalDateTime;
+
+public class Alarm {
+    Long idx;
+    String title;
+    String content;
+    LocalDateTime regDate;
+}
---
+++ board/src/main/java/khj/side/alarm/entity/Receiver.java
@@ -0,0 +1,12 @@
+package khj.side.alarm.entity;
+
+import java.time.LocalDateTime;
+
+public class Receiver {
+    Long idx;
+    Long userIdx;
+    LocalDateTime regDate;
+    LocalDateTime readDate;
+
+    Alarm alarm;
+}
